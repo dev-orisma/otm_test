f08a1c0c4e0d5a68f128a4ab3b58ef263c98283f
Nuttanun Waewpatch <mookoo777@gmail.com>
2016-12-02 09:50:14 +0700

    update

--- test_home.js
+++ test_home.js
@@ -3,6 +3,6 @@ describe('หน้าหลัก', function() {
     it('ทดสอบ', function () {
         browser.url('http://otm.orisma.alpha/');
         var title = browser.getTitle();
-        assert.equal(title, 'OTM');
+        assert.equal(title, 'Orisma Task Manager');
     });
 });
